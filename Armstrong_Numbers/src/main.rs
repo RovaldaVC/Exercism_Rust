a5d6949cc3bb1f5de0edfcd19451da3946b96db8
Arshia <rovaldavc@gmail.com>
2025-10-17 16:36:37 +0330

    Armstrong_number exercise added

---
+++ Armstrong_Numbers/src/main.rs
@@ -0,0 +1,12 @@
+pub fn is_armstrong_number(num: u32) -> bool {
+    let s = num.to_string();
+    let l = s.len() as u32;
+    
+    let total = s
+        .chars()
+        .map(|c| c.to_digit(10).unwrap().pow(l))
+        .sum();
+    num == total
+}
+
+fn main(){}
