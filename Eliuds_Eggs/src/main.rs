641c908913892bacafba78fdccfe26ea265470a0
Arshia <rovaldavc@gmail.com>
2025-11-03 16:08:20 +0330

    Eliuds_Eggs added

---
+++ Eliuds_Eggs/src/main.rs
@@ -0,0 +1,13 @@
+pub fn egg_count(display_value: u32) -> usize {
+    let mut counter:u32 = 0;
+    let mut value = display_value;
+    while value != 0{
+        if (value & 1) == 1{
+            counter += 1;
+        }
+        value = value >> 1;
+    }
+    counter as usize
+}
+
+fn main() {}
